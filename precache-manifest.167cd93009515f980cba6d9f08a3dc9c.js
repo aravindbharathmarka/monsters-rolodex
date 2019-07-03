d2c3ee177d3a19427fe38788d9b51a0700c9209f
Aravind <aravindbharathmarka@gmail.com>
2019-07-03 08:46:51 +0530

    Updates

---
+++ precache-manifest.167cd93009515f980cba6d9f08a3dc9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "973bf70ad9a841e7780ed5443b7f60ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e1478f364d8e0108bd22",
+    "url": "/monsters-rolodex/static/css/main.05a47dfd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e1478f364d8e0108bd22",
+    "url": "/monsters-rolodex/static/js/main.9fe2f676.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
